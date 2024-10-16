7a42c90ab539d797bb66b6d46d919d19173d8865
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-10-15 22:08:56 -0300

    "Added MongoDB and CORS dependencies, updated package.json, and modified app.ts, routes.ts, server.ts, swagger.js, and swagger_output.json files"

--- switchy_api/swagger.js
+++ switchy_api/swagger.js
@@ -1,6 +1,6 @@
 const swaggerAutogen = require('swagger-autogen')()
-
+const port = process.env.PORT
 const outputFile = './swagger_output.json'
 const endpointsFiles = ['./src/routes.ts']
 
-swaggerAutogen(outputFile, endpointsFiles)
+swaggerAutogen(outputFile, endpointsFiles, {host: `localhost:3333`})
