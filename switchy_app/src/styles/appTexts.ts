d2dea6463a37104930c85bc2d2fb218c98d623b9
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-10-17 15:52:58 -0300

    "Removed styles import and styles object, simplified App component"

---
+++ switchy_app/src/styles/appTexts.ts
@@ -0,0 +1,43 @@
+import appColors from "./appColors";
+import type { TextStyle } from "react-native";
+
+const title1SemiBold: TextStyle = {
+    fontFamily: "Roboto",
+    fontWeight: "semibold",
+    fontSize: 24,
+    color: appColors.text100,
+};
+const title2Medium: TextStyle = {
+    fontFamily: "Roboto",
+    fontWeight: "medium",
+    fontSize: 16,
+    color: appColors.text100,
+};
+const paragraph1Regular: TextStyle = {
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    fontSize: 14,
+    color: appColors.text100,
+};
+const paragraph2Regular: TextStyle = {
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    fontSize: 13,
+    color: appColors.text100,
+};
+const paragraph3Regular: TextStyle = {
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    fontSize: 10,
+    color: appColors.text100,
+};
+
+const textStyles = {
+    title1SemiBold,
+    title2Medium,
+    paragraph1Regular,
+    paragraph2Regular,
+    paragraph3Regular,
+};
+
+export default textStyles;
