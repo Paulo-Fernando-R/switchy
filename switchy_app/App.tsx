90824d6efdf3cec412fe8d5b5b46128a3b55bf73
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-10-17 15:56:36 -0300

    "Added import for appColors and updated StatusBar with custom backgroundColor and style"

--- switchy_app/App.tsx
+++ switchy_app/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import appColors from "./src/styles/appColors";
 
 export default function App() {
     return (
         <View>
             <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <StatusBar backgroundColor={appColors.bg100} style="light" />
         </View>
     );
 }
