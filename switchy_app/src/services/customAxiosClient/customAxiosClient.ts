e6d9ee27185b0604ae136b99952dadc2748aa4f6
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-10-18 16:38:22 -0300

    Add `.env` to `.gitignore` file.

---
+++ switchy_app/src/services/customAxiosClient/customAxiosClient.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse, AxiosError, InternalAxiosRequestConfig } from "axios";
+
+const url = "10.0.0.135";
+
+const instance = axios.create({
+    baseURL: url,
+});
+
+const reqInterceptor = async (config: InternalAxiosRequestConfig<AxiosResponse>) => {
+    config.validateStatus = () => true;
+    return config;
+};
+
+const resInterceptor = (response: AxiosResponse) => {
+    if (response.status === 401) {
+        //todo
+    }
+    return response;
+};
+
+const errInterceptor = async (error: AxiosError) => {
+    //  const {  } = error;
+    //todo
+    return Promise.reject(error);
+};
+instance.interceptors.response.use(resInterceptor);
+instance.interceptors.request.use(reqInterceptor, errInterceptor);
+
+const axiosInstance = instance;
+
+export default axiosInstance;
