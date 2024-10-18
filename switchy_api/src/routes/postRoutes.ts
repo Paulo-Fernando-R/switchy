d4cd603ac9ace1d31563ed402261a82c5070ff2f
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-10-18 13:13:53 -0300

    Remove two commented lines from postRoutes.ts

--- switchy_api/src/routes/postRoutes.ts
+++ switchy_api/src/routes/postRoutes.ts
@@ -5,8 +5,6 @@ import jwtMiddleware from "../middleware/jwtMiddleware";
 const postRoutes = Router();
 const controller = new PostController();
 
-//!Mudar parametro para pegar do token depois
-
 postRoutes.post("/CreatePost/", jwtMiddleware.veryfyJWT, (req, res) => {
     // #swagger.tags = ['Post']
     // #swagger.responses[200] = { description: 'Ok.' }
