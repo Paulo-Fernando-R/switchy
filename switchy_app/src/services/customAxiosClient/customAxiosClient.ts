72ff37ce537450bb97d951e157c770f844a36e6b
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-10-20 12:40:17 -0300

    "Refactor custom Axios client to use class implementation with interceptors"

--- switchy_app/src/services/customAxiosClient/customAxiosClient.ts
+++ switchy_app/src/services/customAxiosClient/customAxiosClient.ts
@@ -1,35 +1,54 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import axios, { AxiosResponse, AxiosError, InternalAxiosRequestConfig } from "axios";
+import axios, { AxiosResponse, AxiosError, InternalAxiosRequestConfig, AxiosInstance } from "axios";
+import ICustomAxiosClient from "./IcustomAxiosClient";
 
 const tempToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzEzYTMzZTdhMmJhMmMxYmVhNjMxY2QiLCJpYXQiOjE3MjkzNDAyMjIsImV4cCI6MTcyOTQyNjYyMn0.gcsiRCzBBn7WmTg1fHhvpZF3ZHey15PCRpG7-ODFcWA";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzE1MWM1ZWM3ZTU2NWJmNjdiOTQ2MjkiLCJpYXQiOjE3Mjk0MzY3NjYsImV4cCI6MTcyOTUyMzE2Nn0.o5wJu_b9lA4axdPBB5soTZXfGdT5iWCP93UKM2LkOFU";
 
-const instance = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_API_URL,
-    headers: {
-        Authorization: tempToken,
-    },
-});
 
-const reqInterceptor = async (config: InternalAxiosRequestConfig<AxiosResponse>) => {
-    config.validateStatus = () => true;
-    return config;
-};
 
-const resInterceptor = (response: AxiosResponse) => {
-    if (response.status === 401) {
-        //todo
+export default class CustomAxiosClient implements ICustomAxiosClient {
+    instance: AxiosInstance;
+
+    constructor() {
+        this.instance = axios.create({
+            baseURL: process.env.EXPO_PUBLIC_API_URL,
+            headers: {
+                Authorization: tempToken,//mudar para storage
+            },
+        });
+
+        this.instance.interceptors.request.use(this.reqInterceptor(), this.errorInterceptor());
+        this.instance.interceptors.response.use(this.resInterceptor());
     }
-    return response;
-};
 
-const errInterceptor = async (error: AxiosError) => {
-    //  const {  } = error;
-    //todo
-    return Promise.reject(error);
-};
-instance.interceptors.response.use(resInterceptor);
-instance.interceptors.request.use(reqInterceptor, errInterceptor);
+    private resInterceptor() {
+        const interceptor = (response: AxiosResponse) => {
+            if (response.status === 401) {
+                //todo
+            }
+            return response;
+        };
 
-const axiosInstance = instance;
-export default axiosInstance;
+        return interceptor;
+    }
+
+    private reqInterceptor() {
+        const interceptor = async (config: InternalAxiosRequestConfig<AxiosResponse>) => {
+            config.validateStatus = () => true;
+            return config;
+        };
+
+        return interceptor;
+    }
+
+    private errorInterceptor() {
+        const interceptor = async (error: AxiosError) => {
+            //  const {  } = error;
+            //todo
+            return Promise.reject(error);
+        };
+
+        return interceptor;
+    }
+}
