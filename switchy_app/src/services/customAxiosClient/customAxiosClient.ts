125839ac248ff3d3392783f1702e296746540d78
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-10-21 13:28:58 -0300

    "Refactored CustomAxiosClient to use StorageService for token storage and retrieval"

--- switchy_app/src/services/customAxiosClient/customAxiosClient.ts
+++ switchy_app/src/services/customAxiosClient/customAxiosClient.ts
@@ -1,25 +1,33 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios, { AxiosResponse, AxiosError, InternalAxiosRequestConfig, AxiosInstance } from "axios";
 import ICustomAxiosClient from "./IcustomAxiosClient";
+import StorageService from "../storageService/storageService";
+import StorageTypeEnum from "../../enums/storageTypeEnum";
+import IStorageService from "../storageService/IstorageService";
+import Auth from "../../models/auth";
 
 const tempToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzE1MWM1ZWM3ZTU2NWJmNjdiOTQ2MjkiLCJpYXQiOjE3Mjk0MzY3NjYsImV4cCI6MTcyOTUyMzE2Nn0.o5wJu_b9lA4axdPBB5soTZXfGdT5iWCP93UKM2LkOFU";
-
-
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzEwMDkzNGY4MTNmY2I5MjA3YmU0YjUiLCJpYXQiOjE3Mjk1MjQ3MTMsImV4cCI6MTcyOTYxMTExM30.WwKXgMqkeB8iIFW7gwPgu9tbdHxKaeu7VbgQZSGE4C4";
 
 export default class CustomAxiosClient implements ICustomAxiosClient {
     instance: AxiosInstance;
-
+    storage: IStorageService<Auth>;
     constructor() {
+        this.storage = new StorageService<Auth>(StorageTypeEnum.auth);
         this.instance = axios.create({
             baseURL: process.env.EXPO_PUBLIC_API_URL,
             headers: {
-                Authorization: tempToken,//mudar para storage
+                Authorization: this.getToken(), 
             },
         });
 
         this.instance.interceptors.request.use(this.reqInterceptor(), this.errorInterceptor());
         this.instance.interceptors.response.use(this.resInterceptor());
+    }
+
+    private getToken() {
+        const res = this.storage.getItem();
+        return res?.accessToken;
     }
 
     private resInterceptor() {
