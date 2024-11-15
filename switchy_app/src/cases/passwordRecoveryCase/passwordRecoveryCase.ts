be3ec7ae4b6bcfd25e79c99d353aa41d939bee5c
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-11-15 15:05:38 -0300

    "Added BadRequestError import and email validation with error handling in PasswordRecoveryCase"

--- switchy_app/src/cases/passwordRecoveryCase/passwordRecoveryCase.ts
+++ switchy_app/src/cases/passwordRecoveryCase/passwordRecoveryCase.ts
@@ -1,4 +1,4 @@
-import { CustomError } from "../../errors/customErrors";
+import { BadRequestError, CustomError } from "../../errors/customErrors";
 import AuthRepository from "../../repositories/authRepository/authRepository";
 import IAuthRepository from "../../repositories/authRepository/IauthRepository";
 
@@ -9,12 +9,12 @@ export default class PasswordRecoveryCase {
         this.repository = new AuthRepository();
     }
     async execute(email: string) {
-        //const reg = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
-        // if (!reg.test(email)) {
-        //     return;
-        // }
-        //!adicionar validações
+        const reg = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
+
         try {
+            if (!reg.test(email)) {
+                throw new BadRequestError(400, "", "Email inválido");
+            }
             await this.repository.recovery(email);
         } catch (error) {
             console.error(error);
