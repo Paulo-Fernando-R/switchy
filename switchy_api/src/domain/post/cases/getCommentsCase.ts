574dc32753d700f51405eb6c144cce32fe301e32
Caíque Figueiredo de Araújo <caique.ara19@gmail.com>
2024-11-20 18:28:52 -0300

    Fix likes and comments on getCommentsByPostId

--- switchy_api/src/domain/post/cases/getCommentsCase.ts
+++ switchy_api/src/domain/post/cases/getCommentsCase.ts
@@ -1,6 +1,7 @@
 import { Types } from "mongoose";
 import IPostRepository from "../../../repositories/postRepository/IpostRepository";
 import { IPost } from "../../../models/post";
+import GetCommentsByPostResponse from "../response/getCommentsByPostResponse";
 
 export default class GetCommentsCase {
     private readonly postRepository: IPostRepository;
@@ -9,7 +10,7 @@ export default class GetCommentsCase {
         this.postRepository = postRepository;
     }
 
-    async execute(id: string): Promise<IPost[]> {
+    async execute(id: string, userId: string): Promise<IPost[]> {
         const post = await this.postRepository.getPostById(id);
         const comments = post.comments;
         if (!comments || comments.length == 0) {
@@ -18,6 +19,8 @@ export default class GetCommentsCase {
 
         const ids = comments.map((e) => new Types.ObjectId(e.postId));
         const posts = await this.postRepository.getPostComments(ids);
-        return posts;
+        const response = new GetCommentsByPostResponse(posts);
+        response.setPostsLikedByUser(userId);
+        return response.getResponse();
     }
 }
---
+++ switchy_api/src/domain/post/response/getCommentsByPostResponse.ts
@@ -0,0 +1,35 @@
+import { IPost } from "../../../models/post";
+
+export default class GetCommentsByPostResponse {
+    private posts: any[];
+    constructor(posts: Array<IPost>) {
+        this.posts = posts;
+    }
+
+    public getLenghtOfPostsArray(): void {
+        const convertedPosts: any = this.posts.map((post: any) => {
+            post.comments = Array.isArray(post.comments) ? post.comments?.length : post.comments;
+            post.likes = Array.isArray(post.likes) ? post.likes?.length : post.likes;
+            return post;
+        })
+    
+        this.posts = convertedPosts;
+    }
+
+    public setPostsLikedByUser(userId: any): void {
+
+        const posts = this.posts.map((post: any) => {
+            post.likedByUser = false;
+            if (post.likes.filter((like: any) => like.userId.equals(userId)).length > 0) {
+                post.likedByUser = true
+            }
+            return post
+        })
+        this.posts = posts;
+    }
+
+    public getResponse(){
+        this.getLenghtOfPostsArray();
+        return this.posts
+    }
+}
