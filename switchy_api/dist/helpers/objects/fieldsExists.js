51414c482d68a831a5aae157b5beca0c6da16c0d
Matheus Ferreira <matheus.44300@gmail.com>
2024-11-23 19:22:39 -0300

    Delete switchy_api/dist directory

--- switchy_api/dist/helpers/objects/fieldsExists.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = fieldsExists;
-function fieldsExists(fields, rules) {
-    return rules.every(rule => fields.hasOwnProperty(rule));
-}
--- switchy_api/dist/services/crypto/generatePasswordService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto_1 = require("crypto");
-class GeneratePasswordService {
-    generateHex(length) {
-        return (0, crypto_1.randomBytes)(length / 2).toString('hex');
-    }
-}
-exports.default = GeneratePasswordService;
