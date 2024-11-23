d7ef34d221b3058435a3bc5345f6ca45391b30e0
Matheus Ferreira <matheus.44300@gmail.com>
2024-11-23 19:24:33 -0300

    Delete switchy_api/dist directory

--- switchy_api/dist/errors/serverError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ServerError extends Error {
-    constructor(message = "internal server error", code = 500) {
-        super(message);
-        this.code = code;
-    }
-}
-exports.default = ServerError;
--- switchy_api/dist/helpers/objects/fieldsNotEmpty.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = fieldsNotEmpty;
-function fieldsNotEmpty(fields) {
-    return Object.values(fields).every(value => value !== "");
-}
