84da1f8beff39ae599f2bfc0dcc43b68dabb3488
Matheus Ferreira <matheus.44300@gmail.com>
2024-11-23 17:00:11 -0300

    Removed use case from createPostCase

--- switchy_api/src/domain/post/cases/createPostCase.ts
+++ switchy_api/src/domain/post/cases/createPostCase.ts
@@ -1,9 +1,6 @@
-import { Types } from "mongoose";
 import { IPost } from "../../../models/post";
 import IPostRepository from "../../../repositories/postRepository/IpostRepository";
 import { PostEmptyValueError, UnableCreatePostError } from "../errors/postErrors";
-import GetUserByIdCase from "../../user/cases/getUserByIdCase";
-import { UserRepository } from "../../../repositories/userRepository/userRepository";
 import IPostUser from "../../../models/postUser";
 
 export default class CreatePostCase {
@@ -13,21 +10,10 @@ export default class CreatePostCase {
         this.postRepository = _postRepository;
     }
 
-    async execute(parentId: string, content: string, userId: string) {
+    async execute(parentId: string, content: string, postUser: IPostUser) {
         if (!content) {
             throw new PostEmptyValueError();
         }
-        const user = await new GetUserByIdCase(new UserRepository()).execute(userId);
-
-        if (!user) {
-            throw new UnableCreatePostError();
-        }
-
-        var postUser: IPostUser = {
-            id: new Types.ObjectId(user.id.toString()),
-            name: user.name,
-            userName: user.userName,
-        };
 
         const post: IPost = {
             user: postUser,
@@ -41,7 +27,6 @@ export default class CreatePostCase {
         try {
             await this.postRepository.createPost(post);
         } catch (error) {
-            console.error(error);
             throw new UnableCreatePostError();
         }
     }
