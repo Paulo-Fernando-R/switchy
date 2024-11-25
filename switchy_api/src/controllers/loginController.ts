71aa78cf346d914e657ddee9adee87aee6fbddbb
Matheus Ferreira <matheus.44300@gmail.com>
2024-11-25 11:26:20 -0300

    Fix login controller

--- switchy_api/src/controllers/loginController.ts
+++ switchy_api/src/controllers/loginController.ts
@@ -5,22 +5,14 @@ import { UserError, UserNotFoundError } from "../domain/user/errors/userErrors";
 import GenerateTokenFromUserCase from "../domain/auth/cases/generateTokenFromUserCase";
 import GetUserFromTokenCase from "../domain/auth/cases/getUserFromTokenCase";
 import { AuthEmptyFieldsError, AuthInvalidTokenError } from "../domain/auth/errors/authErrors";
-import ITokenService from "../services/token/itokenService";
-import JwtTokenService from "../services/token/jwtTokenService";
-import IUserRepository from "../repositories/userRepository/IuserRepository";
-import { UserRepository } from "../repositories/userRepository/userRepository";
 import container from "../injection";
 
 export default class LoginController {
-    private readonly tokenService: ITokenService;
-    private readonly userRepository: IUserRepository;
     private readonly getUserByEmailPasswordCase: GetUserByEmailPasswordCase;
     private readonly generateTokenFromUserCase: GenerateTokenFromUserCase;
     private readonly getUserFromTokenCase: GetUserFromTokenCase;
     
     constructor() {
-        this.tokenService = new JwtTokenService();
-        this.userRepository = new UserRepository();
         this.getUserByEmailPasswordCase = container.get<GetUserByEmailPasswordCase>('GetUserByEmailPasswordCase');
         this.generateTokenFromUserCase = container.get<GenerateTokenFromUserCase>('GenerateTokenFromUserCase');
         this.getUserFromTokenCase = container.get<GetUserFromTokenCase>('GetUserFromTokenCase');
@@ -34,16 +26,16 @@ export default class LoginController {
             const response = await this.generateTokenFromUserCase.execute(user);
 
             res.type("application/json").status(StatusCodes.Ok).send(response);
-        } catch (ex) {
-            if (ex instanceof UserNotFoundError) {
+        } catch (err) {
+            if (err instanceof UserNotFoundError) {
                 res.status(StatusCodes.NotFound).send();
                 return;
-            } else if (ex instanceof UserError) {
+            } else if (err instanceof UserError) {
                 res.status(StatusCodes.BadRequest).send();
                 return;
             }
 
-            throw ex;
+            throw err;
         }
     }
 
