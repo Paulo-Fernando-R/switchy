890562029e0f61c2ea03dc8d5fd89fb7651eff80
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-11-27 10:47:10 -0300

    "Updated server to use dynamic IP address and refactored swagger configuration"

---
+++ switchy_api/src/utils/moduleServerIp.js
@@ -0,0 +1,22 @@
+const os = require("os");
+
+ const getServerIp = () => {
+    for (let devName in os.networkInterfaces()) {
+        let iface = os.networkInterfaces()[devName];
+        for (let i = 0; i < iface.length; i++) {
+            let alias = iface[i];
+
+            console.log(alias);
+
+            if (alias.family === "IPv4" && (alias.address === "127.0.0.1" || alias.internal)) {
+                return "localhost";
+            }
+
+            if (alias.family === "IPv4") {
+                return alias.address;
+            }
+        }
+    }
+};
+
+module.exports = { getServerIp };
