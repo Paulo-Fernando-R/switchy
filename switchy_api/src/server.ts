675f2077227146920b629e708ad48d18884c9960
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2024-12-05 18:53:53 -0300

    fixed env variables bug

--- switchy_api/src/server.ts
+++ switchy_api/src/server.ts
@@ -1,15 +1,14 @@
-//@ts-ignore
-import swaggerFile from "../swagger_output.json";
-import { getServerIp } from "./utils/esServerIp";
-import swaggerUi from "swagger-ui-express";
 import dotenv from "dotenv";
-import app from "./app";
-
-
 const env = process.env.NODE_ENV;
 dotenv.config({
     path: env?.includes("development") ? ".env.dev" : ".env.prod",
 });
+
+//@ts-ignore
+import swaggerFile from "../swagger_output.json";
+import { getServerIp } from "./utils/esServerIp";
+import swaggerUi from "swagger-ui-express";
+import app from "./app";
 
 const PORT = process.env.PORT;
 
