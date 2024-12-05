43b9d195040d646b384f379b181190e61cdbdd08
Matheus Ferreira <matheus.44300@gmail.com>
2024-12-05 15:55:36 -0300

    Update

--- switchy_api/src/routes/notificationsRoutes.ts
+++ switchy_api/src/routes/notificationsRoutes.ts
@@ -5,7 +5,7 @@ const notificationsRoutes = Router();
 const notificationsController = new NotificationsController();
 
 notificationsRoutes.get('/ByDate/:lastDate', (req, res) => {
-    // #swagger.tags = ['Comments']
+    // #swagger.tags = ['Notifications']
     // #swagger.responses[200] = { description: 'Ok.' }
     // #swagger.responses[400] = { description: 'Bad Request.' }
     // #swagger.responses[401] = { description: 'Unauthorized.' }
